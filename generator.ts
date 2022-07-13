4046ffb25b92b00bac9942393b380b535061b163
Wei-Han He <scsa32@gmail.com>
2022-07-13 17:35:54 +0800

    Initial commit

---
+++ generator.ts
@@ -0,0 +1,14 @@
+function getMajor() {
+    for (let arrayElement of Array(10)) {
+        console.log(arrayElement)
+    }
+    // for (const i of Array() {
+    //     let card = document.createElement("p");
+    //     let zerofilled = ('00'+i).slice(-2);
+    //     let text = document.createTextNode(zerofilled);
+    //     card.appendChild(text);
+    //     console.log(card)
+    // }
+}
+
+getMajor()
