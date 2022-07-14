67b862ac9fdda88d08e0b24baf6293e5954519df
Wei-Han He <scsa32@gmail.com>
2022-07-14 16:00:42 +0800

    Remove test parts

--- test.js
+++
@@ -1,9 +0,0 @@
-function getMajor() {
-    let majors = [];
-    for (const i of Array(22).keys()) {
-        let zeroFilled = ('00' + i).slice(-2);
-        let name = 'maj' + zeroFilled + '.jpg';
-        majors.push(name);
-    }
-    return majors;
-}
